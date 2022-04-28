979785e4ba539a30b4d3796e1334122ff84551c0
Pas92 <pavelburdavitsyn92@gmail.com>
2022-04-28 15:29:28 +0300

    feat: add solution for carbon-dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,14 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string') return false;
+  let input = parseFloat(sampleActivity);
+  if (input <= 0 || input >= MODERN_ACTIVITY)  return false;
+  if (isNaN(input)) return false;
+  return Math.ceil(((Math.log(MODERN_ACTIVITY / input)) / (0.693 / HALF_LIFE_PERIOD))) || false;
+  // throw new NotImplementedError('Not implemented');
+  // // remove line with error and write your code here
 }
 
 module.exports = {
