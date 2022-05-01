ac2b7dda89bfd67ea2c20006e62d643adf797595
Pas92 <pavelburdavitsyn92@gmail.com>
2022-05-01 23:44:08 +0300

    feat: add solutin for matrix-elements-sum exercise

--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -16,9 +16,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getMatrixElementsSum(matrix) {
+  let sum = 0;
+  let rows = matrix.length;
+  let columns = matrix[0].length;
+  let isSum = true;
+
+  console.log(rows);
+  console.log(columns);
+
+  for (let i = 0; i < columns; i++) {
+    for (let j = 0; j < rows; j++) {
+      console.log(matrix[j][i]);
+      if(matrix[j][i] === 0) {
+        isSum = false;
+      } else if (isSum){
+        sum += matrix[j][i];
+      }
+    }
+    isSum = true;
+  }
+
+  return sum;
+  // throw new NotImplementedError('Not implemented');
+  // // remove line with error and write your code here
 }
 
 module.exports = {
